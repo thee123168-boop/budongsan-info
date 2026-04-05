a5607589f4db759fc73b72c7104385e76230080e
Rio <thee123168@gmail.com>
2026-04-06 01:13:52 +0900

    Add adsense code and 15 posts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2612560508912506"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>
         <Header />
         <main>{children}</main>
